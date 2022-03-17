33cbd3c0f7da12b515fe6b9f5a8c185059868503
Kyunghwan Ko <64265107+kyunghwan1207@users.noreply.github.com>
2022-03-17 19:51:50 +0900

    Add Python File [1244_스위치 켜고 끄기.py]
    스위치의 상태를 바꿔주는 반복적인 작업을 change함수를 통해 구현함.
    학생의 성별에 따른 스위치 상태변화 과정을 직관적으로 코드로 구현함.

---
+++ BOJ/[BOJ] 시뮬레이션/1244_스위치 켜고 끄기.py
@@ -0,0 +1,49 @@
+import sys, math
+
+input = sys.stdin.readline
+
+# switch[i]값을 반대로 바꿔주는 함수
+def change(x):
+    if x == 0:
+        x = 1
+    else:
+        x = 0
+    return x
+
+'''start of main'''
+n = int(input())
+initial = [-math.inf] # -2147000000
+temp = list(map(int, input().split())) # 켜짐: 1, 꺼짐: 0
+switch = initial + temp # 1번 스위치 == index: 1에 위치하도록 하기 위함
+student_count = int(input())
+# 남: 1, 여: 2
+for _ in range(student_count):
+    gender, s = map(int, input().split()) # 성별, 받는 스위치개수
+    if gender == 1: # 남학생일 경우
+        for i in range(1, n+1):
+            if i%s == 0:
+                switch[i] = change(switch[i])
+
+    else: # 여학생일 경우
+        left = s-1
+        right = s+1
+        max_loop = int(n/2 + 0.5) -1
+        switch[s] = change(switch[s])
+        for _ in range(max_loop):
+            # n=9-> 최대 4번, n=8-> 최대 3번([3, 5], [2, 6], [1, 7])
+            if left == 0 or right == n+1:
+                break
+            if switch[left] == switch[right]: # 대칭이라면
+                switch[left] = change(switch[left])
+                switch[right] = change(switch[right])
+                left -= 1
+                right += 1
+for i in range(1, n+1):
+    if i%20 == 0:
+        print(switch[i])
+    else:
+        if i == n:
+            print(switch[i])
+        else:
+            print(switch[i], end=' ')
+'''end of main'''

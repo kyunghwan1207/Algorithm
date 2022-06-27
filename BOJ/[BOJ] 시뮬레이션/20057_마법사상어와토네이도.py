9be08be98df11bd634d8ca01cadf90b6fe2bd1c9
Kyunghwan Ko <64265107+kyunghwan1207@users.noreply.github.com>
2022-06-27 17:54:30 +0900

    Refactor: 분산시 좌표값을 west 기준으로 규칙적용 후 반복되는 코드 감소시킴.
    main에서, spread()함수 적용 시 무한루프와 exit()코드로 main을 종료하는 대신 이동방향의 최대횟수인 2*n-1만큼 순회하는 코드로 수정
    문제에서 제시된 west방향으로 이동할때 좌표와 모래가 분산되는 퍼센트를 튜플형태로 기록 후 east, north, south 방향으로 이동할때 규칙을 적용해서 불필요한 코드의 반복을 줄임
    
    spread()함수에서 분산되고 나서 나머지 모래양인 remain_sand를 일일이 계산하지 않고 for문을 통해 board[nr][nc]에 기록할때 reamin_sand에서 -= 해줌으로써 효율적으로 남은 모래의 양을 구함.

--- BOJ/[BOJ] 시뮬레이션/20057_마법사상어와토네이도.py
+++ BOJ/[BOJ] 시뮬레이션/20057_마법사상어와토네이도.py
@@ -1,82 +1,58 @@
-# 2230시작 2430완료 정답
+import sys
 
-# 이동하려는 위치 (r,c)가 격자 범위를 벗어나는지/아닌지 -> T/F
-def OOB(r, c):
+def OOB(x, y):
     global n
-    return r < 0 or r >= n or c < 0 or c >= n
+    return x < 0 or x >= n or y < 0 or y >= n
 
-#입력: 문제에서 x표시의 좌표 r, c와 이동한 방향 d
 def spread(r, c, d):
-    global board, ans, dr, dc # ans가 최종정답값
-    # d-> 0 w, 1 s, 2 e, 3 n
-    du = (d+1+4) % 4 # x기준 위방향(n)
-    dd = (d-1+4) % 4 # x기준 아래방향(s)
-    nr, nc = r + dr[d], c + dc[d] # y좌표
-    sand = board[r][c] + board[nr][nc]
+    global board, n, dr, dc, spread_direction_list, ans
+    dirs = spread_direction_list[d]
+    remain_sand = board[r][c] # 일정 p로 분배 후 남은 모래양 기록
+    for dir in dirs:
+        x, y, p = dir[0], dir[1], dir[2]
+        nr, nc = r+x, c+y
+        if OOB(nr, nc):
+            ans += int(board[r][c]*p)
+            remain_sand -= int(board[r][c]*p)
+            continue
+        board[nr][nc] += int(board[r][c]*p)
+        remain_sand -= int(board[r][c]*p)
+    nr, nc = r+dr[d], c+dc[d] # 문제상 a표시 좌표로 이동
+    if OOB(nr, nc):
+        ans += remain_sand
+    else:
+        board[nr][nc] += remain_sand
     board[r][c] = 0
-    # 1% 처리
-    nru, ncu = r + dr[du], c + dc[du]
-    nrd, ncd = r + dr[dd], c + dc[dd]
-    if OOB(nru, ncu): ans += int(sand*0.01)
-    else: board[nru][ncu] += int(sand*0.01)
-    if OOB(nrd, ncd): ans += int(sand*0.01)
-    else: board[nrd][ncd] += int(sand * 0.01)
-    # 2%, 7% 처리
-    nru, ncu = nru + dr[d], ncu + dc[d]
-    nrd, ncd = nrd + dr[d], ncd + dc[d]
-    nruu, ncuu = nru+dr[du], ncu+dc[du]
-    nrdd, ncdd = nrd+dr[dd], ncd+dc[dd]
-    if OOB(nru, ncu): ans += int(sand*0.07)
-    else: board[nru][ncu] += int(sand*0.07)
-    if OOB(nrd, ncd): ans += int(sand * 0.07)
-    else: board[nrd][ncd] += int(sand*0.07)
-    if OOB(nruu, ncuu): ans += int(sand*0.02)
-    else: board[nruu][ncuu] += int(sand*0.02)
-    if OOB(nrdd, ncdd): ans += int(sand * 0.02)
-    else: board[nrdd][ncdd] += int(sand*0.02)
-    # 10% 처리
-    sand_remain = sand - (2 * int(sand * 0.01) + 2 * int(sand * 0.02) + 2 * int(sand * 0.07) + 2 * int(sand * 0.10) + int(sand * 0.05))  # y에 있는 모래의 양
-    nnr, nnc = nr+dr[d], nc+dc[d] # a위치
-    board[nr][nc] = 0 # y의 모래 이동을 위해 기존의 모래없애기
-    if OOB(nnr, nnc): ans += int(sand_remain) # a가 격자를 벗어날 경우
-    else: board[nnr][nnc] += int(sand_remain)
-    nru, ncu = nru + dr[d], ncu + dc[d]
-    nrd, ncd = nrd + dr[d], ncd + dc[d]
-    if OOB(nru, ncu): ans += int(sand*0.10)
-    else: board[nru][ncu] += int(sand*0.10)
-    if OOB(nrd, ncd): ans += int(sand * 0.10)
-    else: board[nrd][ncd] += int(sand*0.10)
-    # 5% 처리
-    nnnr, nnnc = nnr + dr[d], nnc + dc[d]
-    if OOB(nnnr, nnnc): ans += int(sand*0.05)
-    else: board[nnnr][nnnc] += int(sand*0.05)
+
 
 if __name__ == "__main__":
+    input = sys.stdin.readline
     n = int(input())
     board = []
-    ans = 0 # 격자를 벗어난 모래의 양
     for _ in range(n):
         board.append(list(map(int, input().split())))
-    sr, sc = n//2, n//2 # 시작점
-    #     w     s     e    n
-    dr = [0,    1,    0,    -1]
-    dc = [-1,   0,    1,    0]
-    # n=7, 1 1 2 2 3 3 4 4 5 5 6 6
-    step, t, d = 1, 0, 0
-    while True:
-        for _ in range(step):
-            spread(sr, sc, d % 4)
-            sr, sc = sr + dr[d%4], sc + dc[d%4]
-            if sr == 0 and sc == 0: print(ans); exit(0)
-        d += 1
-        t += 1
-        if t % 2 == 0:
-            step += 1
-'''
-느낀점
-1. step은 방향d로 나아갈 횟수를 의미함
-하지만 step이 1, 1, 2, 2, 3, 3 ... 와 같이 임의의 패턴을 가지고 있어서
-t라는 변수를 별도로 두어서 방향d로의 이동이 완료될때 마다 t++시켜서 
-t가 짝수일 경우에 step ++시키는 방법으로 구현
-'''
+    nr, nc = n//2, n//2
+    #     w   n   e  s
+    dr = [0,  1,  0,  -1]
+    dc = [-1,  0,  1,  0]
+    # 문제의 y표시 기준으로 좌표이동값 설정
+    west = [(-1, 1, 0.01), (1, 1, 0.01), (-1, 0, 0.07), (1, 0, 0.07), (-2, 0, 0.02), (2, 0, 0.02),
+            (-1, -1, 0.1), (1, -1, 0.1), (0, -2, 0.05)]
+    north = [(y*-1, x, p) for x, y, p in west]
+    south = [(x*-1, y*-1, p) for x, y, p in north]
+    east = [(x*-1, y*-1, p) for x, y, p in west]
+    spread_direction_list = [west, north, east, south]
+    step, k, d = 0, 0, 0
+    ans = 0
 
+    for i in range(2*n-1):
+        d = i%4
+        if d == 0 or d == 2:
+            step += 1
+        for _ in range(step):
+            temp = board[nr][nc]
+            board[nr][nc] = 0
+            nr, nc = nr + dr[d], nc + dc[d]
+            board[nr][nc] += temp
+            spread(nr, nc, d)
+    print(ans)
